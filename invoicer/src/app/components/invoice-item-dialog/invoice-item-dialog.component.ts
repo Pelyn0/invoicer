b20d7f6177b531df2c6a98288a5ad4f3f0583c79
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-04-13 16:18:14 +0300

    fix

--- invoicer/src/app/components/invoice-item-dialog/invoice-item-dialog.component.ts
+++ invoicer/src/app/components/invoice-item-dialog/invoice-item-dialog.component.ts
@@ -22,7 +22,10 @@ export class InvoiceItemDialogComponent implements OnInit {
   
   onSelectedCarChanged(newValue: string) {
     let selected = this.cars.find(c=>c.selectedCar==newValue);
-    
+
+    console.log(newValue);
+    console.log(selected);
+
     if(selected){
       this.data.title = selected?.title;
       this.data.price = selected?.price;
