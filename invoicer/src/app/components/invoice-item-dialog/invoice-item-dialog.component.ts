23ce3fdba52645b3928533fa57fb0b893b36d6a8
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-04-13 13:24:36 +0300

    Test selector

--- invoicer/src/app/components/invoice-item-dialog/invoice-item-dialog.component.ts
+++ invoicer/src/app/components/invoice-item-dialog/invoice-item-dialog.component.ts
@@ -2,12 +2,24 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { InvoiceItem } from 'src/app/models/invoice-item';
 
+cars: Car[] = [
+    {value: 'volvo', viewValue: 'Volvo'},
+    {value: 'saab', viewValue: 'Saab'},
+    {value: 'mercedes', viewValue: 'Mercedes'},
+  ];
+
 @Component({
   selector: 'app-invoice-item-dialog',
   templateUrl: './invoice-item-dialog.component.html',
   styleUrls: ['./invoice-item-dialog.component.css'],
 })
 export class InvoiceItemDialogComponent implements OnInit {
+  selectedCar: string;
+  cars: Car[] = [
+    {value: 'volvo', viewValue: 'Volvo'},
+    {value: 'saab', viewValue: 'Saab'},
+    {value: 'mercedes', viewValue: 'Mercedes'},
+  ];
   constructor(
     public dialogRef: MatDialogRef<InvoiceItemDialogComponent>,
     @Inject(MAT_DIALOG_DATA)
