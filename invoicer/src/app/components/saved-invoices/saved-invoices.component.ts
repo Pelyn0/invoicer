cef1a4f0aef9481c8eff22ecd6c7058e0344be58
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-04-19 22:04:47 +0300

    t

--- invoicer/src/app/components/saved-invoices/saved-invoices.component.ts
+++ invoicer/src/app/components/saved-invoices/saved-invoices.component.ts
@@ -61,8 +61,7 @@ export class SavedInvoicesDialogComponent implements OnInit {
   }
 
   getFileName(name: string) {
-    console.log(name);
-    return name.split(/\/(.*)/s)[1];
+    return name.split('.').slice(0, -1).join('');
   }
 
   onNoClick(): void {
