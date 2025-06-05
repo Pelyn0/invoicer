14d165d4e87f38df56f8cbdb7252bdfc3a3f08c6
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-06-05 23:30:17 +0300

    update timeline

--- invoicer/src/app/components/timeline/timeline.component.ts
+++ invoicer/src/app/components/timeline/timeline.component.ts
@@ -9,22 +9,39 @@ import { Component, OnInit } from "@angular/core";
 export class TimelineComponent implements OnInit {
     timelineData = [
         {
-        title: 'Created Invoice',
-        date: new Date('2024-01-01'),
-        description: 'Invoice #001 was created.'
+            title: 'Title 0',
+            description: 'This is the description of the event 0',
+            date: new Date('2025-06-05T19:24:00')
         },
         {
-        title: 'Payment Received',
-        date: new Date('2024-01-05'),
-        description: 'Customer made a full payment.'
+            title: 'Title 2',
+            description: 'This is the description of the event 2',
+            date: new Date('2025-06-05T19:24:00')
         },
         {
-        title: 'Invoice Closed',
-        date: new Date('2024-01-06'),
-        description: 'Invoice marked as completed.'
+            title: 'Title 1',
+            description: 'This is the description of the event 1',
+            date: new Date('2025-06-06T19:24:00')
+        },
+        {
+            title: 'Title 3',
+            description: 'This is the description of the event 3',
+            date: new Date('2025-06-06T19:24:00')
+        },
+        {
+            title: 'Title 4',
+            description: 'This is the description of the event 4',
+            date: new Date('2025-07-05T19:24:00')
         }
     ];
 
     ngOnInit(): void {
+    }
+
+    showDateLabel(index: number): boolean {
+        if (index === 0) return true;
+        const prev = this.timelineData[index - 1].date.toDateString();
+        const current = this.timelineData[index].date.toDateString();
+        return prev !== current;
     }
 }
