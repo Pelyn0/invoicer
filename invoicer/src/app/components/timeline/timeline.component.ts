6f59d989c9f2b3237e6d6051f53be1a889f6308e
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-07-05 15:47:24 +0300

    timeline by location

--- invoicer/src/app/components/timeline/timeline.component.ts
+++ invoicer/src/app/components/timeline/timeline.component.ts
@@ -11,29 +11,36 @@ export class TimelineComponent implements OnInit {
         {
             title: 'Title 0',
             description: 'This is the description of the event 0',
-            date: new Date('2025-06-05T19:24:00')
+            date: new Date('2025-06-05T19:24:00'),
+            location: "Rest"
         },
         {
             title: 'Title 2',
             description: 'This is the description of the event 2',
-            date: new Date('2025-06-05T19:24:00')
+            date: new Date('2025-06-05T19:24:00'),
+            location: "Rest"
         },
         {
             title: 'Title 1',
             description: 'This is the description of the event 1',
-            date: new Date('2025-06-06T19:24:00')
+            date: new Date('2025-06-06T19:24:00'),
+            location: "Rest"
         },
         {
             title: 'Title 3',
             description: 'This is the description of the event 3',
-            date: new Date('2025-06-06T19:24:00')
+            date: new Date('2025-06-06T19:24:00'),
+            location: "Home"
         },
         {
             title: 'Title 4',
             description: 'This is the description of the event 4',
-            date: new Date('2025-07-05T19:24:00')
+            date: new Date('2025-07-05T19:24:00'),
+            location: "Home"
         }
     ];
+
+    timelineCategories: string[] = this.timelineData.reduce(t=>location);
     
     ngOnInit(): void {
     }
