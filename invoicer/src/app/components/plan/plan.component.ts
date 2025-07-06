92d378c38e76bef60ffd33c8b2b969180b672a7e
Taras Pelyno <51337585+Pelyn0@users.noreply.github.com>
2025-07-06 13:43:59 +0300

    test

--- invoicer/src/app/components/plan/plan.component.ts
+++ invoicer/src/app/components/plan/plan.component.ts
@@ -28,27 +28,32 @@ export class PlanComponent implements OnInit {
         {
             title: 'Title 0',
             description: 'This is the description of the event 0',
-            date: new Date('2025-06-05T19:24:00')
+            date: new Date('2025-06-05T19:24:00'),
+            location: "Rest"
         },
         {
             title: 'Title 2',
             description: 'This is the description of the event 2',
-            date: new Date('2025-06-05T19:24:00')
+            date: new Date('2025-06-05T19:24:00'),
+            location: "Rest"
         },
         {
             title: 'Title 1',
             description: 'This is the description of the event 1',
-            date: new Date('2025-06-06T19:24:00')
+            date: new Date('2025-06-06T19:24:00'),
+            location: "Home"
         },
         {
             title: 'Title 3',
             description: 'This is the description of the event 3',
-            date: new Date('2025-06-06T19:24:00')
+            date: new Date('2025-06-06T19:24:00'),
+            location: "Home"
         },
         {
             title: 'Title 6',
             description: 'This is the description of the event 4',
-            date: new Date('2025-07-05T19:24:00')
+            date: new Date('2025-07-05T19:24:00'),
+            location: "Home"
         }
     ].sort((a, b) => a.date.getTime() - b.date.getTime());
 
